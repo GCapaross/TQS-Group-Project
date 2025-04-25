70c6f87639471e1e5eeee527ff9f46193b7a9b57
GCapaross <gvds@ua.pt>
2025-04-25 17:11:50 +0100

    Frontend start

---
+++ frontend/ChargingPlatform/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Stations from './pages/Stations';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/stations" element={<Stations />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App; 
---
+++ frontend/ChargingPlatform/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to EV Charging Platform</h1>
+      <p>Find and book charging stations near you</p>
+    </div>
+  );
+}
+
+export default Home; 
