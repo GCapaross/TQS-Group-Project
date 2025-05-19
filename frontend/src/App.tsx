3a2405fc8a6136d5a7136ca9c174428f2f815b6f
GCapaross <gvds@ua.pt>
2025-05-19 12:28:45 +0100

    Undone

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,11 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Stations from "./pages/Stations";
 import ChargingStationMap from './pages/ChargingStationMap';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import BookingPage from './pages/BookingPage';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { AuthProvider } from './contexts/AuthContext';
 import "./App.css";
 
 const theme = createTheme({
@@ -16,24 +20,39 @@ const theme = createTheme({
       main: '#dc004e',
     },
   },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+          borderRadius: 8,
+        },
+      },
+    },
+  },
 });
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
-        <div className="App">
-          <Navbar />
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/stations" element={<Stations />} />
-              <Route path="/map" element={<ChargingStationMap />} />
-            </Routes>
+      <AuthProvider>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/stations" element={<Stations />} />
+                <Route path="/map" element={<ChargingStationMap />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/stations/:stationId/book" element={<BookingPage />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
