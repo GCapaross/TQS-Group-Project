5c5a573681777f13d6d6615cf6d030583a86cba5
Affapple <joaopmgaspar@ua.pt>
2025-05-24 22:31:30 +0100

    Removed MAINTENACE state for charger

--- chargingplatform/src/main/java/nikev/group/project/chargingplatform/model/Charger.java
+++ chargingplatform/src/main/java/nikev/group/project/chargingplatform/model/Charger.java
@@ -3,8 +3,8 @@ package nikev.group.project.chargingplatform.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Data
@@ -15,26 +15,26 @@ import lombok.Setter;
 @AllArgsConstructor
 @Table(name = "chargers")
 public class Charger {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "status", nullable = false)
-    private ChargerStatus status;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(name = "charging_speed_kw")
-    private double chargingSpeedKw;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "status", nullable = false)
+  private ChargerStatus status;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "station_id")
-    private Station station;
+  @Column(name = "charging_speed_kw")
+  private double chargingSpeedKw;
 
-    public static enum ChargerStatus {
-        AVAILABLE,      // Charger is free and operational
-        CHARGING,       // Charger is currently in use
-        RESERVED,       // Charger is reserved for a user
-        OUT_OF_SERVICE, // Charger is not operational (e.g., broken)
-        MAINTENANCE     // Charger is under maintenance
-    }
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "station_id")
+  private Station station;
+
+  public static enum ChargerStatus {
+    AVAILABLE, // Charger is free and operational
+    CHARGING, // Charger is currently in use
+    RESERVED, // Charger is reserved for a user
+    OUT_OF_SERVICE, // Charger is not operational (e.g., broken)
+  }
 }
