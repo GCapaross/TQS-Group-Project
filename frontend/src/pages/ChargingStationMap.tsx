a7ce73fc86db5a67024f6ec80ffdb2e4417730c3
Shelton Lázio Agostinho <sheltonagostinho@ua.pt>
2025-06-05 00:29:52 +0100

    correct atribute

--- frontend/src/pages/ChargingStationMap.tsx
+++ frontend/src/pages/ChargingStationMap.tsx
@@ -274,7 +274,7 @@ const ChargingStationMap: React.FC = () => {
                                             Price: ${station.pricePerKwh}/kWh
                                         </Typography>
                                         <Typography variant="body2">
-                                            Speed: {station.chargerSpeeds[0]} kW
+                                            Speed: {station.chargingSpeedKw[0]} kW
                                         </Typography>
                                         <Box sx={{ mt: 1, display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
                                             {station.supportedConnectors.map((type) => (
