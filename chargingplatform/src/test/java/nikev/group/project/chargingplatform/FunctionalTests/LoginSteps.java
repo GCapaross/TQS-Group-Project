c6d7a4336b13606c61b26bef818ff040c8f9b62a
Affapple <joaopmgaspar@ua.pt>
2025-06-04 21:04:53 +0100

    Removed unnecessary comment

--- chargingplatform/src/test/java/nikev/group/project/chargingplatform/FunctionalTests/LoginSteps.java
+++ chargingplatform/src/test/java/nikev/group/project/chargingplatform/FunctionalTests/LoginSteps.java
@@ -2,8 +2,16 @@ package nikev.group.project.chargingplatform.FunctionalTests;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import java.time.Duration;
-
+import nikev.group.project.chargingplatform.model.User;
+import nikev.group.project.chargingplatform.service.UserService;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -16,79 +24,71 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import nikev.group.project.chargingplatform.model.User;
-import nikev.group.project.chargingplatform.service.UserService;
-
 public class LoginSteps {
-  private WebDriver driver;
-  private static final String BASE_URL = "http://localhost:5173";
 
-  @Before
-  public void setUp() {
-    WebDriverManager.chromedriver().setup();
-    ChromeOptions options = new ChromeOptions();
-    
-    // Remove --headless for debugging
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--disable-gpu");
-    options.addArguments("--remote-debugging-port=9222");
-    options.addArguments("--window-size=1920,1080");
-    
-    driver = new ChromeDriver(options);
-  }
+    private WebDriver driver;
+    private static final String BASE_URL = "http://localhost:5173";
 
-  @After
-  public void closeUp() {
-    driver.close();
-  }
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
 
-  @Autowired
-  private UserService userService;
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-debugging-port=9222");
+        options.addArguments("--window-size=1920,1080");
 
-  @Given("an account with email {string} and password {string}")
-  public void givenTheUserIsOnTheHomepage(String email, String password) {
-    User user = new User();
-    user.setEmail(email);
-    user.setPassword(password);
-    user.setName("Test User");
-    userService.registerUser(user);
-  }
+        driver = new ChromeDriver(options);
+    }
 
-  @And("the user is on the login page")
-  public void andTheUserIsOnTheLoginPage() {
-    driver.get(BASE_URL + "/login");
-  }
+    @After
+    public void closeUp() {
+        driver.close();
+    }
 
-  @When("the user enters the email {string} and password {string}")
-  public void whenTheUserEntersTheEmailAndPassword(
-    String email,
-    String password
-  ) {
-    WebElement emailField = driver.findElement(By.id("email"));
-    WebElement passwordField = driver.findElement(By.id("password"));
+    @Autowired
+    private UserService userService;
 
-    emailField.sendKeys(email);
-    passwordField.sendKeys(password);
-  }
+    @Given("an account with email {string} and password {string}")
+    public void givenTheUserIsOnTheHomepage(String email, String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setName("Test User");
+        userService.registerUser(user);
+    }
 
-  @And("the user clicks on the login button")
-  public void andTheUserClicksTheLoginButton() {
-    WebElement loginButton = driver.findElement(By.id("login-button"));
-    loginButton.click();
-  }
-  
-  @Then("the user should be redirected to the homepage")
-  public void thenTheUserShouldBeRedirectedToTheHomepage() {
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-    wait.until(ExpectedConditions.not(ExpectedConditions.urlContains("/login")));
-    assertThat(driver.getCurrentUrl()).isEqualTo(BASE_URL + "/");
-  }
+    @And("the user is on the login page")
+    public void andTheUserIsOnTheLoginPage() {
+        driver.get(BASE_URL + "/login");
+    }
+
+    @When("the user enters the email {string} and password {string}")
+    public void whenTheUserEntersTheEmailAndPassword(
+        String email,
+        String password
+    ) {
+        WebElement emailField = driver.findElement(By.id("email"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+
+        emailField.sendKeys(email);
+        passwordField.sendKeys(password);
+    }
+
+    @And("the user clicks on the login button")
+    public void andTheUserClicksTheLoginButton() {
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
+    }
+
+    @Then("the user should be redirected to the homepage")
+    public void thenTheUserShouldBeRedirectedToTheHomepage() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(
+            ExpectedConditions.not(ExpectedConditions.urlContains("/login"))
+        );
+        assertThat(driver.getCurrentUrl()).isEqualTo(BASE_URL + "/");
+    }
 }
